ac0d10e5084a9e0a4c5c1073445802a914e8cd71
hxllv <nace.tavcer20@gmail.com>
2023-05-31 22:24:19 +0200

    seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for ($i = 15; $i < 100; $i++) {
+            \App\Models\User::factory()->create([
+                "name" => "Nace$i",
+                "surname" => "Tavcer$i",
+                "email" => "nace.tavcer+$i@gmail.com",
+                "role_id" => 2
+            ]);
+        }
+    }
+}
