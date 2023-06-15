bade56e630a74e0c398d977f40fdcacbfc9c26e4
hxllv <nace.tavcer20@gmail.com>
2023-06-15 21:27:43 +0200

    rename

--- database/migrations/2023_06_07_154833_create_school_classes_table.php
+++ database/migrations/2023_06_07_154833_create_school_classes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('school_classes', function (Blueprint $table) {
             $table->id();
-            $table->string('class_name');
+            $table->string('name');
             $table->foreignId('user_id');
             $table->timestamps();
         });
